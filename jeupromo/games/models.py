89aadb30042de54f7439c7d17c5231a918c0913e
Dex Morgan <meyer2012@hotmail.fr>
2018-07-12 18:10:47 +0200

    modification models.py

--- jeupromo/games/models.py
+++ jeupromo/games/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Player(models.Model):
-    pseudo = models.Charfield(verbose_name= 'pesudo', unique=True)
-
-class Session(models.Model):
-    game = models.Foreignkey(Games,on_delete=models.CASCADE)
-    player = models.Foreignkey(Player,on_delete=models.CASCADE)
-    datetime = models.datetime
-    reward = models.Foreignkey(Reward,on_delete=models.CASCADE)
-    result = models.Charfield(max_length=150)
-
-
+    pseudo = models.CharField(max_length=100, verbose_name= 'pseudo', unique=True)
 
 class Reward(models.Model):
-    label = models.Charfield(max_length=150)
-    value = models.Integerfield(max_length=4)
+    label = models.CharField(max_length=150)
+    value = models.IntegerField()
 
 class Games(models.Model):
-    label = models.Charfield(max_length=150)
-    game_type = models.Charfield(max_length=150)
-git 
+    label = models.CharField(max_length=150)
+    game_type = models.CharField(max_length=150)
+
+class Session(models.Model):
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE)
+    result = models.CharField(max_length=150)
+
+
+
