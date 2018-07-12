fb121d34cab1ad94aedd42ef795fd0fec04bc031
rudy vovanqui <rudy.vovanqui@yahoo.fr>
2018-07-12 17:44:29 +0200

    first settings

--- jeupromo/jeupromo/settings.py
+++ jeupromo/jeupromo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'games'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-europe'
 
 TIME_ZONE = 'UTC'
 
