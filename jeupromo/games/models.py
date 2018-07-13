eb55a6bf5200c71172b7303e2bcffdabbe654e85
Dex Morgan <meyer2012@hotmail.fr>
2018-07-13 11:05:38 +0200

    ajout def __str__ et class Meta

--- jeupromo/games/models.py
+++ jeupromo/games/models.py
@@ -4,20 +4,42 @@ from django.db import models
 class Player(models.Model):
     pseudo = models.CharField(max_length=100, verbose_name= 'pseudo', unique=True)
 
+    def __str__(self):
+        return self.pseudo
+
+    class Meta:
+        verbose_name = "Joueur"
+
 class Reward(models.Model):
     label = models.CharField(max_length=150)
     value = models.IntegerField()
+
+    def __str__(self):
+        return self.label + ' ' + str(self.value)
+
+    class Meta:
+        verbose_name = "Gain"
 
 class Games(models.Model):
     label = models.CharField(max_length=150)
     game_type = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        verbose_name = "Jeux"
+
+
 class Session(models.Model):
     game = models.ForeignKey(Games, on_delete=models.CASCADE)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(default=datetime.now())
     reward = models.ForeignKey(Reward, on_delete=models.CASCADE)
     result = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.player.pseudo + ' ' + self.game.label + ' ' + str(self.datetime) + ' ' + self.reward.label + ' ' + self.result
 
-
+    class Meta:
+        verbose_name = "Session"
