a3eb0054a28f7081a00b19195ec09812640e56e3
Leo <razak30@hotmail.fr>
2018-07-19 08:37:03 +0200

    ajout des vues playerlist playerDetail gamelist

--- jeupromo/games/urls.py
+++ jeupromo/games/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from . import views
+from .views import gamelist2, playerDetail
 
 app_name = 'games'
 
 urlpatterns = [
     path('home', views.home, name = 'home'),
-    path('gamelist', views.gamelist, name='gamelist')
+    path('gamelist', views.gamelist, name='gamelist'),
+    path('playerlist', views.playerlist, name='playerlist'),
+    path('gamelist2', gamelist2.as_view(), name='gamelist2'),
+    path('playerdetail/<int:pk>/', playerDetail.as_view(), name='playerDetail')
 ]
--- jeupromo/games/views.py
+++ jeupromo/games/views.py
@@ -15,3 +15,25 @@ def home(request):
 def gamelist(request):
     games = Games.objects.all()
     return render(request, 'games/affichageJeux.html', {'games':games})
+
+def playerlist(request):
+    players = Player.objects.all()
+    return render(request, 'games/playerlist.html', {'players':players})
+
+
+class gamelist2(ListView):
+    model = Games
+    template_name = 'games/gamelist2.html'
+
+class playerDetail(DetailView):
+    context_object_name = 'player'
+    queryset = Player.objects.all()
+    template_name = 'game/playerDetail.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['players'] = Player.objects.all()
+        return context
+
